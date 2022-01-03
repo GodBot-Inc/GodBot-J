37ad4f6f5ffcfbd7c385bbf2ddddf703d55977ea
Kai <kaithomas2001@gmail.com>
2022-01-03 15:43:16 +0100

    changed gitignore to ignore binaries

--- src/main/java/commands/Play.java
+++ src/main/java/commands/Play.java
@@ -21,7 +21,6 @@ import snippets.InterpretationKeys;
 import snippets.TrackInfoKt;
 import utils.*;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -35,30 +34,45 @@ public class Play implements Command {
     public static void playPlaylist(
             AudioPlayerExtender audioPlayer,
             List<String> videoIds,
-            Member member
+            Member member,
+            boolean shuffle
     ) {
         AudioPlayerManager audioPlayerManager = AudioPlayerManagerWrapper
                 .getInstance()
                 .getManager();
 
-        for (String videoId : videoIds) {
-            AudioResultHandler audioResultHandler = new AudioResultHandler(
-                    audioPlayer,
-                    member
-            );
+        videoIds.remove(0);
 
-            audioPlayerManager.loadItem(
-                    String.format(
-                            "https://www.youtube.com/watch?v=%s",
-                            videoId
-                    ),
-                    audioResultHandler
-            );
+        if (!shuffle) {
+            for (String videoId : videoIds) {
+                AudioResultHandler audioResultHandler = new AudioResultHandler(
+                        audioPlayer,
+                        member
+                );
 
-            while (audioResultHandler.actionType == 0) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(20);
-                } catch (InterruptedException ignore) {}
+                audioPlayerManager.loadItem(
+                        String.format(
+                                "https://www.youtube.com/watch?v=%s",
+                                videoId
+                        ),
+                        audioResultHandler
+                );
+
+                while (audioResultHandler.actionType == 0) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(20);
+                    } catch (InterruptedException ignore) {}
+                }
+            }
+        } else {
+            for (String videoId : videoIds) {
+                audioPlayerManager.loadItem(
+                        String.format(
+                                "https://www.youtube.com/watch?v=%s",
+                                videoId
+                        ),
+                        new AudioResultHandler(audioPlayer, member)
+                );
             }
         }
     }
@@ -71,6 +85,11 @@ public class Play implements Command {
         }
 
         return url.getAsString();
+    }
+
+    private static boolean getShuffle(EventExtender event) {
+        OptionMapping shuffle = event.event.getOption("shuffle");
+        return shuffle != null && shuffle.getAsBoolean();
     }
 
     public static Future<HashMap<String, PlayableInfo>> startInterpretation(String url) {
@@ -98,12 +117,185 @@ public class Play implements Command {
         return audioResultHandler;
     }
 
-    private static void processPlaylist() {
-        // TODO
+    private static void processPlaylist(
+            String url,
+            AudioPlayerExtender player,
+            Member member,
+            boolean shuffle,
+            InteractionHook interactionHook,
+            Future<HashMap<String, PlayableInfo>> playableFuture,
+            JDA bot,
+            Guild guild,
+            VoiceChannel voiceChannel
+    ) {
+        HashMap<String, PlayableInfo> playlistInformation;
+        try{
+            playlistInformation =
+                    playableFuture.get();
+        } catch (InterruptedException e) {
+            interactionHook
+                    .sendMessageEmbeds(
+                            ErrorsKt.standardError(
+                                    ErrorMessages.INFO_GATHERING_PLAYLIST_FAILED
+                            )
+                    ).queue();
+            return;
+        } catch (ExecutionException e) {
+            if (e.getCause() instanceof InvalidURLException) {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(
+                                        ErrorMessages.INVALID_URL
+                                )
+                        ).queue();
+                return;
+            } else if (e.getCause() instanceof PlatformNotFoundException) {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(
+                                        ErrorMessages.PLATFORM_NOT_FOUND
+                                )
+                        ).queue();
+                return;
+            }
+            e.printStackTrace();
+            interactionHook
+                    .sendMessageEmbeds(
+                            ErrorsKt.standardError(
+                                    ErrorMessages.INFO_GATHERING_PLAYLIST_FAILED
+                            )
+                    ).queue();
+            return;
+        }
+
+        YouTubePlaylist youTubePlaylist;
+
+        if (!playlistInformation.containsKey(InterpretationKeys.YTPLAYLIST)) {
+            interactionHook
+                    .sendMessageEmbeds(
+                            ErrorsKt.standardError(
+                                    ErrorMessages.INFO_GATHERING_PLAYLIST_FAILED
+                            )
+                    ).queue();
+            return;
+        } else {
+            youTubePlaylist = (YouTubePlaylist) playlistInformation.get(InterpretationKeys.YTPLAYLIST);
+        }
+
+        int positionInQueue = player.getQueue().size() + 1;
+        AudioPlayerExtender audioPlayer = AudioPlayerManagerWrapper
+                .getInstance()
+                .getPlayer(
+                        bot,
+                        guild.getId(),
+                        voiceChannel
+                );
+
+        Executors.newCachedThreadPool().submit(() -> playPlaylist(
+                audioPlayer,
+                youTubePlaylist.getVideoIds(),
+                member,
+                shuffle
+        ));
+
+        interactionHook
+                .sendMessageEmbeds(
+                        TrackInfoKt.playPlaylist(
+                                member,
+                                youTubePlaylist,
+                                positionInQueue,
+                                positionInQueue
+                        )
+                ).queue();
     }
 
-    private static void processVideo() {
-        // TODO
+    private static void processVideo(
+            String url,
+            AudioPlayerExtender player,
+            Member member,
+            InteractionHook interactionHook,
+            Future<HashMap<String, PlayableInfo>> playableFuture
+    ) {
+        AudioResultHandler audioResultHandler = playVideo(
+                player,
+                url,
+                member
+        );
+
+        while (audioResultHandler.actionType == 0) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(200);
+            } catch (InterruptedException ignore) {}
+        }
+
+        switch (audioResultHandler.actionType) {
+            case 10 -> {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(ErrorMessages.GENERAL_ERROR)
+                        ).queue();
+                return;
+            }
+            case 3 -> {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(ErrorMessages.VIDEO_NOT_FOUND)
+                        ).queue();
+                return;
+            }
+            case 4 -> {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(ErrorMessages.LOADING_FAILED)
+                        ).queue();
+                return;
+            }
+        }
+
+        HashMap<String, PlayableInfo> playableInfo;
+        try {
+            playableInfo = playableFuture.get();
+        } catch (InterruptedException e) {
+            interactionHook
+                    .sendMessageEmbeds(
+                            ErrorsKt.standardError(ErrorMessages.INFO_GATHERING_SONG_FAILED)
+                    ).queue();
+            return;
+        } catch (ExecutionException e) {
+            if (e.getCause() instanceof InvalidURLException) {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(
+                                        ErrorMessages.INVALID_URL
+                                )
+                        ).queue();
+                return;
+            } else if (e.getCause() instanceof PlatformNotFoundException) {
+                interactionHook
+                        .sendMessageEmbeds(
+                                ErrorsKt.standardError(
+                                        ErrorMessages.PLATFORM_NOT_FOUND
+                                )
+                        ).queue();
+                return;
+            }
+            interactionHook
+                    .sendMessageEmbeds(
+                            ErrorsKt.standardError(
+                                    ErrorMessages.INFO_GATHERING_SONG_FAILED
+                            )
+                    ).queue();
+            return;
+        }
+
+        interactionHook.sendMessageEmbeds(
+                TrackInfoKt.playVideo(
+                        member,
+                        audioResultHandler.position == 0,
+                        playableInfo,
+                        player.getQueue().size() + 1,
+                        player.getQueue().size() + 1
+                )).queue();
     }
 
     public static void trigger(@NotNull SlashCommandEvent scEvent) {
@@ -204,192 +396,25 @@ public class Play implements Command {
         }
 
         if (!isVideo) {
-            String firstUrl = null;
-            try {
-                firstUrl = LinkInterpreter.getFirst(url);
-            } catch (IOException | RequestException ignore) {}
-            catch (InvalidURLException e) {
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(ErrorMessages.INVALID_URL)
-                        ).queue();
-                return;
-            } catch (PlatformNotFoundException e) {
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(ErrorMessages.PLATFORM_NOT_FOUND)
-                        ).queue();
-                return;
-            }
-
-            if (player.getAudioPlayer().getPlayingTrack() == null) {
-                if (firstUrl != null) {
-                    Executors.newCachedThreadPool().submit(() -> playVideo(
-                            player,
-                            url,
-                            member
-                    ));
-                }
-            }
-
-            HashMap<String, PlayableInfo> songInformation;
-            try{
-                songInformation =
-                        interpretationFuture.get();
-            } catch (InterruptedException e) {
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(
-                                        ErrorMessages.INFO_GATHERING_SONG_FAILED
-                                )
-                        ).queue();
-                return;
-            } catch (ExecutionException e) {
-                if (e.getCause() instanceof InvalidURLException) {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(
-                                            ErrorMessages.INVALID_URL
-                                    )
-                            ).queue();
-                    return;
-                } else if (e.getCause() instanceof PlatformNotFoundException) {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(
-                                            ErrorMessages.PLATFORM_NOT_FOUND
-                                    )
-                            ).queue();
-                    return;
-                }
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(
-                                        ErrorMessages.INFO_GATHERING_PLAYLIST_FAILED
-                                )
-                        ).queue();
-                return;
-            }
-
-            YouTubePlaylist youTubePlaylist;
-
-            if (!songInformation.containsKey(InterpretationKeys.YTPLAYLIST)) {
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(
-                                        ErrorMessages.INFO_GATHERING_PLAYLIST_FAILED
-                                )
-                        ).queue();
-                return;
-            } else {
-                youTubePlaylist = (YouTubePlaylist) songInformation.get(InterpretationKeys.YTPLAYLIST);
-            }
-
-            int positionInQueue = player.getQueue().size() + 1;
-            AudioPlayerExtender audioPlayer = AudioPlayerManagerWrapper
-                    .getInstance()
-                    .getPlayer(
-                            bot,
-                            guild.getId(),
-                            voiceChannel
-                    );
-
-            Executors.newCachedThreadPool().submit(() -> playPlaylist(
-                    audioPlayer,
-                    youTubePlaylist.getVideoIds(),
-                    member
-            ));
-
-            interactionHook
-                    .sendMessageEmbeds(
-                            TrackInfoKt.playPlaylist(
-                                    member,
-                                    youTubePlaylist,
-                                    positionInQueue,
-                                    positionInQueue
-                            )
-                    ).queue();
-        } else {
-            AudioResultHandler audioResultHandler = playVideo(
-                    player,
+            processPlaylist(
                     url,
-                    member
-            );
-
-            while (audioResultHandler.actionType == 0) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(200);
-                } catch (InterruptedException ignore) {}
-            }
-
-            switch (audioResultHandler.actionType) {
-                case 10 -> {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(ErrorMessages.GENERAL_ERROR)
-                            ).queue();
-                    return;
-                }
-                case 3 -> {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(ErrorMessages.VIDEO_NOT_FOUND)
-                            ).queue();
-                    return;
-                }
-                case 4 -> {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(ErrorMessages.LOADING_FAILED)
-                            ).queue();
-                    return;
-                }
-            }
-
-            HashMap<String, PlayableInfo> playableInfo;
-            try {
-                playableInfo = interpretationFuture.get();
-            } catch (InterruptedException e) {
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(ErrorMessages.INFO_GATHERING_SONG_FAILED)
-                        ).queue();
-                return;
-            } catch (ExecutionException e) {
-                if (e.getCause() instanceof InvalidURLException) {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(
-                                             ErrorMessages.INVALID_URL
-                                    )
-                            ).queue();
-                    return;
-                } else if (e.getCause() instanceof PlatformNotFoundException) {
-                    interactionHook
-                            .sendMessageEmbeds(
-                                    ErrorsKt.standardError(
-                                            ErrorMessages.PLATFORM_NOT_FOUND
-                                    )
-                            ).queue();
-                    return;
-                }
-                interactionHook
-                        .sendMessageEmbeds(
-                                ErrorsKt.standardError(
-                                        ErrorMessages.INFO_GATHERING_SONG_FAILED
-                                )
-                        ).queue();
-                return;
-            }
-
-            interactionHook.sendMessageEmbeds(
-                    TrackInfoKt.playVideo(
+                    player,
                     member,
-                    audioResultHandler.position == 0,
-                    playableInfo,
-                    player.getQueue().size() + 1,
-                    player.getQueue().size() + 1
-            )).queue();
+                    getShuffle(event),
+                    interactionHook,
+                    interpretationFuture,
+                    bot,
+                    guild,
+                    voiceChannel
+            );
+        } else {
+            processVideo(
+                    url,
+                    player,
+                    member,
+                    interactionHook,
+                    interpretationFuture
+            );
         }
     }
 }
