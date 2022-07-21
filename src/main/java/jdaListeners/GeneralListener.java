51017036d92cd9e6ff6b04245c4ad2e68964bdda
kai.kueppers <kai.kueppers@gi.com>
2022-07-21 09:47:49 +0200

    fix: Changed lib to objects import in GeneralListener and removed Checks.java

--- src/main/java/jdaListeners/GeneralListener.java
+++ src/main/java/jdaListeners/GeneralListener.java
@@ -1,7 +1,7 @@
 package jdaListeners;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import lib.AudioPlayerExtender;
+import objects.AudioPlayerExtender;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
