854f7eaf2d6a6a5f1bf2a82d1b8078fae524c846
kai.kueppers <kai.kueppers@gi.com>
2022-07-21 18:38:02 +0200

    refactor: Removed unnecessary AudioManagerVault (guild and AudioManager are interconnected)

--- src/main/java/singeltons/AudioPlayerManagerWrapper.java
+++ src/main/java/singeltons/AudioPlayerManagerWrapper.java
@@ -1,11 +1,11 @@
 package singeltons;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import lib.lavaplayer.AudioResultHandler;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import objects.AudioPlayerExtender;
 
@@ -25,35 +25,30 @@ public class AudioPlayerManagerWrapper {
         playerManager.getConfiguration().setOpusEncodingQuality(AudioConfiguration.OPUS_QUALITY_MAX);
         // set to 1000 if bad (ms not s)
         // Should preload 1 Minute and 20 Seconds
-        playerManager.setFrameBufferDuration((int) TimeUnit.SECONDS.toMillis(80));
+        playerManager.setFrameBufferDuration((int) TimeUnit.SECONDS.toMillis(10));
         // set to 500 if bad
         playerManager.setItemLoaderThreadPoolSize(1000);
         AudioSourceManagers.registerRemoteSources(playerManager);
     }
 
-    private AudioPlayerExtender createPlayer(JDA bot, String guildId, VoiceChannel voiceChannel) {
+    private AudioPlayerExtender createPlayer(JDA bot, Guild guild, VoiceChannel voiceChannel) {
         PlayerVault vault = PlayerVault.getInstance();
         AudioPlayerExtender player =
                 new AudioPlayerExtender(
                         playerManager.createPlayer(),
                         voiceChannel,
-                        AudioManagerVault
-                                .getInstance()
-                                .getAudioManager(
-                                        bot,
-                                        guildId
-                                )
+                        guild.getAudioManager()
                 );
-        vault.storePlayer(bot, guildId, player);
+        vault.storePlayer(bot, guild.getId(), player);
         return player;
     }
 
-    public AudioPlayerExtender getOrCreatePlayer(JDA bot, String guildId, VoiceChannel voiceChannel) {
-        AudioPlayerExtender player = PlayerVault.getInstance().getPlayer(bot, guildId);
+    public AudioPlayerExtender getOrCreatePlayer(JDA bot, Guild guild, VoiceChannel voiceChannel) {
+        AudioPlayerExtender player = PlayerVault.getInstance().getPlayer(bot, guild.getId());
         if (player == null) {
             player = createPlayer(
                     bot,
-                    guildId,
+                    guild,
                     voiceChannel
             );
         }
@@ -65,10 +60,6 @@ public class AudioPlayerManagerWrapper {
                 url,
                 resultHandler
         );
-    }
-
-    public AudioPlayerManager getManager() {
-        return this.playerManager;
     }
 
     public static AudioPlayerManagerWrapper getInstance() {
