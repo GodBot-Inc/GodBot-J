30903643be79a20effa38350b529ad00f5f0add5
kai.kueppers <kai.kueppers@gi.com>
2022-08-15 07:22:56 +0200

    feature: Added scripts

---
+++ scripts/aws-connect.sh
@@ -0,0 +1,2 @@
+mv out/artifacts/GodBot_jar/GodBot.jar scripts/GodBot.jar
+sftp -i scripts/"Lol I actually lost my previous one.pem" ubuntu@ec2-54-88-197-12.compute-1.amazonaws.com
---
+++ scripts/move.sh
@@ -0,0 +1,9 @@
+echo "About to copy Executables"
+
+rm /Volumes/GodBotShare/.env
+cp ../.env /Volumes/GodBotShare/.env
+rm /Volumes/GodBotShare/GodBot.jar
+cp ../out/artifacts/GodBot_jar/GodBot.jar /Volumes/GodBotShare/GodBot.jar
+cp refresh.txt /Volumes/GodBotShare/refresh.txt
+
+echo "Moving Successful"
